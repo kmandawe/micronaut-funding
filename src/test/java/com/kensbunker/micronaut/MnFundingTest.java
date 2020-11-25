39950f050d0332d18c77118d66d8e2cd766089b5
Kenneth Mandawe <kenneth.mandawe@cheetahdigital.com>
2020-11-25 17:04:56 +0800

    Initial commit

---
+++ src/test/java/com/kensbunker/micronaut/MnFundingTest.java
@@ -0,0 +1,19 @@
+package com.kensbunker.micronaut;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import javax.inject.Inject;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+@MicronautTest
+public class MnFundingTest {
+
+  @Inject
+  EmbeddedApplication application;
+
+  @Test
+  void testItWorks() {
+    Assertions.assertTrue(application.isRunning());
+  }
+}
